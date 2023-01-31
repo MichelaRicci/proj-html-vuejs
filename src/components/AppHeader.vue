d1ea768b18bf7e0ac2d5b57b6997fee3f7bfc6ea
MichelaRicci <michela.ricci.1991@gmail.com>
2023-01-31 13:46:15 +0100

    main header added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,65 @@
 <script>
 
+
+
+export default {
+    data() {
+        return {
+            navList: ['Home', 'Recipes', ' Places', 'Blog', 'About', 'Contact']
+        }
+
+    }
+
+}
+
 </script>
 
 <template>
 
+    <header>
+
+        <div><img src="../assets/avada-food-logo.png" alt=""></div>
+        <div class="nav-list">
+            <nav>
+                <ul>
+                    <li v-for="item in navList"><a href="#">{{ item }}</a></li>
+                    <li class="d-inline"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
+
+                </ul>
+            </nav>
+        </div>
+
+
+    </header>
+
 </template>
 
-<style scoped>
+<style lang='scss' scoped>
+img {
+    width: 100px;
+}
 
+ul {
+    display: flex;
+}
+
+nav a {
+
+    padding: 10px;
+    text-decoration: none;
+    color: #67615A;
+
+}
+
+li {
+    list-style-type: none;
+}
+
+.fa-solid {
+
+    color: #67615A;
+    font-size: 15px;
+
+
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).mount('#app')
+
+// Specific Icons
+
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+
+library.add(
+    faMagnifyingGlass,
+)
+
+const app = createApp(App)
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount('#app');
+
+
